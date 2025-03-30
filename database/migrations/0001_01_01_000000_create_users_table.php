4a304049bff5d5210a3317c53b50a5387cd4b93f
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-30 15:29:19 +0300

    users table change for OAuth

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 50)->unique();
+            $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->string('password')->nullable();
+            $table->string('avatar', 255);
+            $table->text('bio');
+            $table->enum("role", ["user", "admin"])->default("user");
             $table->rememberToken();
             $table->timestamps();
         });

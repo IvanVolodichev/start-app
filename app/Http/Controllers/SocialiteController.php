73942343a0db6e03fc9a42d373689b35b060a563
Ivan Volodichev <ivan_vol@icloud.com>
2025-03-30 19:25:00 +0300

    realize oauth with yandex and google

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use SocialiteProviders\Manager\Config;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class SocialiteController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        try {
+            $socialUser = Socialite::driver($provider)->user();
+
+            $user = User::firstOrCreate(
+                ['email' => $socialUser->getEmail()],
+                [
+                    'username' => $socialUser->getName() ?? $socialUser->getNickname(),
+                    'provider' => $provider,
+                    'provider_id' => $socialUser->getId(),
+                    'password' => Str::random(32),
+                    'avatar' => $socialUser->getAvatar(),
+                ]
+            );
+    
+            Auth::login($user);
+            return redirect('/');
+            
+    
+        } catch (\Exception $e) {
+            Log::error("OAuth error: {$e->getMessage()}");
+            dd($e->getMessage());
+        }
+    }
+}
